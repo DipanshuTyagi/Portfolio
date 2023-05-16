4b55746f8bcfa798e19f82fd8cbbdd4c16d77c16
DipanshuTyagi <deepanshutyagi4567@gmail.com>
2023-05-16 16:17:43 +0530

    Second day

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,22 +1,23 @@
-import React from 'react';
-import './home.css';
-import Social from './Social';
-import Data from './Data';
-
+import React from "react";
+import "./home.css";
+import Social from "./Social";
+import Data from "./Data";
+import ScrollDown from "./ScrollDown";
 
 const Home = () => {
   return (
-      <section className="home section" id="home">
-          <div className="home__container container grid">
-              <div className="home__container grid">
-                  <Social />
-                  <div className="home__img">
-                      <Data/>
-                  </div>
-              </div>
-          </div>
-    </section>
-  )
-}
+    <section className="home section" id="home">
+      <div className="home__container container grid">
+        <div className="home__content grid">
+          <Social />
 
-export default Home
+          <div className="home__img"></div>
+          <Data />
+        </div>
+        <ScrollDown />
+      </div>
+    </section>
+  );
+};
+
+export default Home;
